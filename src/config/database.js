e023999ab010a9b6ad59e45ed45efe58eaffa401
Caroline1397 <carololiveirasantos955@gmail.com>
2020-04-24 15:47:27 -0400

    first commit

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+dialect: 'postgres',
+host: 'tuffi.db.elephantsql.com',
+username: 'cdwqsgvr',
+password: 'BhkI9VCylg5llCzg8pdlxQiumRL0BRkK',
+database: 'cdwqsgvr',
+define:{
+    timestamps: true,
+    underscored: true,
+    },
+};
